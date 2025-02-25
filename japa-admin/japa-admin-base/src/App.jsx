50bf01a3c0b32f7628b59574c8a6ff94b3ff3b1e
Victor <samuelvictoralexander@gmail.com>
2025-02-25 16:14:27 +0100

    added wlecome to japa title and logo

--- japa-admin/japa-admin-base/src/App.jsx
+++ japa-admin/japa-admin-base/src/App.jsx
@@ -1,27 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Login";
 import Layout from "./Components/Layout";
 import Home from "./Components/Admin/Home";
 import PostJobs from "./Components/Admin/PostJobs";
 import Courses from "./Components/Admin/Courses";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Talents from "./Components/Admin/Talents";
-
 
 function App() {
   const [count, setCount] = useState(0);
-
 
   return (
     <>
       <ToastContainer />
       <Routes>
-
         <Route path="/" element={<Login />} />
         <Route path="/admin" element={<Layout />}>
           <Route path="" element={<Navigate to="home" />} />
